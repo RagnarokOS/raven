7ed6ebadcacd3149117a3b1c345b04fc8bedc260
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-03-21 11:42:54 -0400

    populate repo

---
+++ config.mk
@@ -0,0 +1,43 @@
+# $Ragnarok: config.mk,v 1.2 2023/10/14 16:49:26 lecorbeau Exp $
+
+include ${TOPDIR}/usr/share/X11/mk/xprogs.mk
+
+# raven version
+VERSION = 0.1
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# Xinerama, comment if you don't want it
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
+
+# freetype
+FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = /usr/include/freetype2
+# OpenBSD (uncomment)
+#FREETYPEINC = ${X11INC}/freetype2
+#MANPREFIX = ${PREFIX}/man
+
+# includes and libs
+INCS = -I${X11INC} -I${FREETYPEINC}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+
+# flags
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${HARDENING_CPPFLAGS}
+#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations ${O_FLAG} ${INCS} ${CPPFLAGS} ${HARDENING_CFLAGS}
+LDFLAGS  = ${LIBS} ${HARDENING_LDFLAGS}
+
+# Solaris
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
+
+# compiler and linker
+CC = cc
